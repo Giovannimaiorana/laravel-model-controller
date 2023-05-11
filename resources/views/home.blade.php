cdbfbe4370ef3d72fb22095d1b45624d705de6e6
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-11 14:13:22 +0200

    aggiunta route in controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.generalLayout')
+@section('page_title','Home Page')
+
+@section('content')
+    
+ 
+    @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>Lista Film</h1>
+</header>
+
+
+<style lang="scss" scoped>
+ h1{
+    text-align: center;
+ }
+</style>
