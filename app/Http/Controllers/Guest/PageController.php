c767fadeaa0c36e4e3e787e22451f78722a0e64f
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-11 14:46:50 +0200

    inserimento datbase

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Guest;
-
+use App\Models\Movie;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,4 +11,9 @@ class PageController extends Controller
     public function index() {
         return view('home');
     }
+    public function movie() {
+        $movies = Movies::all();
+        return view('listmovie');
+    }
 }
+
---
+++ resources/views/listmovie.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.generalLayout')
+@section('page_title','Movie')
+
+@section('content')
+    
+ <h1>lista film</h1>
+    @endsection
